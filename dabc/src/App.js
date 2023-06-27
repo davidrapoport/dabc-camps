c844a11c21d5239ff68ce9837ff472c75eacb8ac
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-06-27 12:07:57 -0600

    added basic Home component, checks if user is logged in

--- dabc/src/App.js
+++ dabc/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./components/Register";
 import Login from "./components/Login";
+import Home from "./components/Home";
 import { app } from "./firebase";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
---
+++ dabc/src/components/Home.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
+const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const auth = getAuth();
+  const navigate = useNavigate();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      //user is logged in, all good
+      console.log("user is logged in: ", user);
+      setIsLoggedIn(true);
+    } else {
+      //user is not logged in, send them to /login
+      navigate("/login");
+    }
+  });
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return isLoggedIn ? (
+    <div>
+      <p>This is the home page</p>
+      <button onClick={logOut}>Log Out</button>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default Home;
