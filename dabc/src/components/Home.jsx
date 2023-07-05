3f26d6d54a539a814e4d940162291c5b99af167d
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-07-05 13:22:24 -0600

    save me!

--- dabc/src/components/Home.jsx
+++ dabc/src/components/Home.jsx
@@ -1,20 +1,31 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const auth = getAuth();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [errorCode, setErrorCode] = useState("no err");
+  const [testText, setTestText] = useState("");
   const navigate = useNavigate();
+
+  const inputRef = useRef();
 
   onAuthStateChanged(auth, (user) => {
     if (user) {
       setIsLoggedIn(true);
+      setErrorCode("this shit is crazy");
     } else {
       navigate("/login");
     }
   });
+
+  const addToDb = (e) => {
+    e.preventDefault();
+    setErrorCode("very Error");
+    return;
+  };
 
   const logOut = () => {
     signOut(auth)
@@ -25,9 +36,28 @@ const Home = () => {
   };
 
   return isLoggedIn ? (
-    <div className="home-page">
-      <p>This is the home page</p>
-      <button onClick={logOut} className="logout-button">Log Out</button>
+    <div>
+      <p className="error-message">{errorCode}</p>
+      <div className="home-page">
+        <form onSubmit={addToDb}>
+          <label>
+            Upload file:
+            <input type="file" ref={inputRef}></input>
+          </label>
+          <input
+            type="text"
+            value={testText}
+            onChange={(e) => {
+              console.log("typing");
+              //setTestText(e.target.value);
+            }}
+          ></input>
+          <button>Submit</button>
+        </form>
+        <button onClick={logOut} className="logout-button">
+          Log Out
+        </button>
+      </div>
     </div>
   ) : (
     <></>
