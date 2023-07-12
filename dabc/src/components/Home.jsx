11cf36794a1c9933f3903f8f86d7b3489df5a671
David Rapoport <drapoport847@gmail.com>
2023-07-12 11:20:42 -0600

    Some UX improvements to the Homepage

--- dabc/src/components/Home.jsx
+++ dabc/src/components/Home.jsx
@@ -1,15 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { read, utils } from "xlsx";
 import { collection, doc, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
+import ClipLoader from "react-spinners/ClipLoader";
 import "./Home.css";
 
 const Home = () => {
   const auth = getAuth();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [errorCode, setErrorCode] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadSuccess, setUploadSuccess] = useState(false);
   const navigate = useNavigate();
   const inputRef = useRef();
 
@@ -26,10 +29,15 @@ const Home = () => {
   const addToDb = async (e) => {
     e.preventDefault();
     setErrorCode("");
+    setUploadSuccess(false);
+    setIsUploading(true);
+
     if (inputRef.current.files.length !== 1) {
+      setIsUploading(false);
       setErrorCode("No file chosen for upload");
       return;
     }
+
     const file = await inputRef.current.files[0].arrayBuffer();
     const wb = read(file);
     const ws = wb.Sheets[wb.SheetNames[0]];
@@ -62,12 +70,15 @@ const Home = () => {
       input,
       output: {},
     };
+
     try {
       await addDoc(collection(db, "forms"), formattedDoc);
-    } catch (e) {
-      console.log(e);
+      setIsUploading(false);
+      setUploadSuccess(true);
+    } catch (error) {
+      setIsUploading(false);
+      setErrorCode(error.message);
     }
-    return;
   };
 
   const logOut = () => {
@@ -75,29 +86,57 @@ const Home = () => {
       .then(() => {
         navigate("/login");
       })
-      .catch((err) => {
-        setErrorCode(err.message);
+      .catch((error) => {
+        setErrorCode(error.message);
       });
   };
 
-  return isLoggedIn ? (
-    <div>
-      <p className="error-message">{errorCode}</p>
-      <div className="home-page">
-        <form onSubmit={addToDb}>
-          <label>
-            Upload file:
-            <input type="file" ref={inputRef} accept=".xlsx,.xls"></input>
-          </label>
-          <button>Submit</button>
-        </form>
-        <button onClick={logOut} className="logout-button">
-          Log Out
-        </button>
+  if (!isLoggedIn) {
+    return <></>;
+  }
+
+  let content = <></>;
+  if (uploadSuccess) {
+    content = (
+      <p className="success-message">
+        Please check back in a few minutes to see the results of your run.
+      </p>
+    );
+  } else {
+    content = (
+      <div>
+        <p className="error-message">{errorCode}</p>
+        <div className="home-page">
+          <form onSubmit={addToDb}>
+            <p>
+              Please upload a file with the following structure: <br />
+              SKU, AL Name, Order Qty
+            </p>
+            <label>
+              Upload file:
+              <input type="file" ref={inputRef} accept=".xlsx,.xls" />
+            </label>
+            {isUploading ? (
+              <div className="spinner-container">
+                <ClipLoader />
+                <p className="upload-message">Uploading...</p>
+              </div>
+            ) : (
+              <button type="submit">Submit</button>
+            )}
+          </form>
+        </div>
       </div>
+    );
+  }
+  return (
+    <div className="body">
+      <h2>DABC Route Finding Tool</h2>
+      {content}
+      <button onClick={logOut} className="logout-button">
+        Log Out
+      </button>
     </div>
-  ) : (
-    <></>
   );
 };
 
